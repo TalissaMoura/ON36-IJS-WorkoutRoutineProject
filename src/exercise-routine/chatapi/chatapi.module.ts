fc3ff07832acceca2c470e9a150c7cbf2d87f8cb
TalissaMouraCW <talissa.ayres@cloudwalk.io>
2024-10-17 17:34:32 -0300

    update ConfigModule

--- src/exercise-routine/chatapi/chatapi.module.ts
+++ src/exercise-routine/chatapi/chatapi.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChatResponseService } from './chat-response.service';
 import { WeatherModule } from 'src/weather/weather.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [WeatherModule],
-  providers: [ChatResponseService],
-  exports: [ChatResponseService]
+  imports: [WeatherModule, ConfigModule.forRoot({isGlobal:true})],
+  providers: [ChatResponseService, ConfigService],
+  exports: [ChatResponseService, ConfigService]
 })
 export class ChatApiModule {}
--- src/weather/weatherapi/weatherapi.module.ts
+++ src/weather/weatherapi/weatherapi.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { WeatherApiService } from './weatherapi.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
-  providers: [WeatherApiService],
-  exports: [WeatherApiService], // Optional: if you want to use it in other modules
+  imports: [HttpModule, ConfigModule.forRoot({isGlobal:true})],
+  providers: [WeatherApiService,ConfigService],
+  exports: [WeatherApiService,ConfigService], // Optional: if you want to use it in other modules
 })
 export class WeatherApiModule {}
